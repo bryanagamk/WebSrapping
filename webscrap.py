55c636b5d8a1664e0783c7496493dea0674b416a
bryankottama <bryan.kottama@smart-it.co.id>
2019-10-20 09:48:20 +0700

    Add brand to scrapping

--- webscrap.py
+++ webscrap.py
@@ -7,19 +7,24 @@ url = "https://www.sociolla.com/search?controller=search&orderby=position&orderw
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "lxml")
-products = soup.find_all("div", "product-item")
+products = soup.find_all("div", "product-item eec-data")
 
 names = []
 prices = []
+brands = []
+print(url)
 for item in products:
+    # print what you find
+    print("\n")
     name = item.find("p", "text-center title")
+    print(name)
     price = item.find("p", "text-center price")
+    print("\n")
+    print(price)
+    brand = item['data-eec-brand'] 
+    print("\n")
+    print(brand)
+    # Store to variable
     names.append(name)
     prices.append(price)
-
-print(url)
-for i, n in enumerate(names):
-    print(names[i].text)
-    print("\n")
-    print(prices[i].text)
-    print("\n")    
+    brands.append(brand)
